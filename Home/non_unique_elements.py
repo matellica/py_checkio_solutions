f0dc866f7bf86ac860ef698554756db6eb2af38c
matellica <35024660+matellica@users.noreply.github.com>
2019-08-06 09:56:15 +0900

    時間測定を仮追加

--- Home/non_unique_elements.py
+++ Home/non_unique_elements.py
@@ -1,8 +1,24 @@
 #!/usr/bin/env checkio --domain=py run non-unique-elements
 # https://py.checkio.org/mission/non-unique-elements/
 
+import time
+import timeit
 
-def checkio(data: list) -> list:
+
+def measure(func):
+    def wrapper(*args, **kargs):
+        start_time = time.perf_counter()
+
+        result = func(*args, **kargs)
+
+        end_time = time.perf_counter()
+        execution_time = end_time - start_time
+        print(f'{func.__name__}: {execution_time}')
+        return result
+    return wrapper
+
+@measure
+def checkio1(data: list) -> list:
     numbers = {}
     for digit in data:
         numbers[digit] = numbers.get(digit, 0) + 1
@@ -17,6 +33,24 @@ def checkio(data: list) -> list:
     return non_uniq_elements
 
 
+@measure
+def checkio2(data: list) -> list:
+    return [digit for digit in data if data.count(digit) > 1]
+
+
+@measure
+def checkio3(data: list) -> list:
+    from collections import Counter
+    counter = Counter(data)
+    return [item for item in data if counter[item] > 1]
+
+
+@measure
+def checkio4(data: list) -> list:
+    counter = set(data)
+    return [item for item in counter if  > 1]
+
+
 def test_checkio():
     #These "asserts" using only for self-checking and not necessary for auto-testing
     assert list(checkio([1, 2, 3, 1, 3])) == [1, 3, 1, 3], "1st example"
@@ -27,4 +61,12 @@ def test_checkio():
 
 
 if __name__ == "__main__":
-    test_checkio()
+    # test_checkio()
+    # lst = list(range(10**1))+[0]
+    lst = list(range(10**4))+[0]
+    checkio1(lst)
+    checkio2(lst)
+    checkio3(lst)
+    # print(timeit.timeit('checkio1(lst)',setup='from __main__ import checkio1, lst'))
+    # checkio1(lst)
+    # print(timeit.timeit('checkio2(lst)','from __main__ import checkio2, lst'))
--- Home/sort_array_by_element_frequency.py
+++ Home/sort_array_by_element_frequency.py
@@ -6,10 +6,18 @@ def frequency_sort(items):
     from collections import Counter
     counter = Counter(items)
     res_list = []
-    for key, val in counter.most_common():
+    for key, val in counter.items():
         for x in range(val):
             res_list.append(key)
     return res_list
+
+
+def frequency_sort2(items):
+    # vlad.bezden
+    from collections import Counter
+    c = Counter(items)
+    result = sorted(c.elements(), key=lambda k: c[k], reverse=True)
+    return result
 
 
 def test_frequency_sort():
@@ -23,4 +31,11 @@ def test_frequency_sort():
 
 if __name__ == '__main__':
     test_frequency_sort()
-    print(frequency_sort([4, 6, 2, 2, 6, 4, 4, 4]))
+    # print(frequency_sort([4, 6, 2, 2, 6, 4, 4, 4]))
+
+
+    from timeit import timeit
+    items = [4,6,2,4,5,6,7,2,3,4,6,5,3,1,4,4,5,6]
+    for f in [frequency_sort, frequency_sort2]:
+        t = timeit(stmt="f(items)", number=100, globals=globals())
+        print(f"{f.__name__} took: {t:.6f}")
